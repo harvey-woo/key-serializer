06eff7957fd9ec14bddab841c504c9a5eecfc98a
Harvey.Woo <harvey.woo@outlook.com>
2018-11-02 18:36:37 +0800

    feat: add set function, and change build command to babel

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,42 @@
 /** @ignore */
 const r = /(?=(?:\[|\.))/
 export type PublicKey = Exclude<PropertyKey, symbol>
-const keySerializer = {
-  parse(str: string): PublicKey[] {
-    return str.split(r)
-      .filter(item => item.trim())
-      .map(item => item[0] === '.' ? item.substr(1) : (item[0] === '[' ? + item.substr(1, item.length - 2) : item))
-  },
-  stringify(arr: PublicKey[]): string {
-    return arr.reduce((memo, item, i) =>
-      memo += typeof item === 'number' ? `[${item}]` : i === 0 ? item : `.${item}`
-    , '') as string
-  },
-  query(target: any, keys?: string | PublicKey[] | null) {
-    if (keys == null || keys === '') {
-      return { target, key: undefined, value: target }
-    }
-    if (typeof keys === 'string') {
-      keys = this.parse(keys) as PublicKey[]
-    } else {
-      keys = keys.slice() as PublicKey[]
-    }
-    let value = target
-    let key: PublicKey | undefined
-    while (keys.length) {
-      target = value
-      key = keys.shift()!
-      // this line may throw when target is undefined, and i do not need handle
-      value = target[key]
-    }
-    return { target, key, value }
-  },
-  get(target: object, keys: string | PublicKey[]): any {
-    return this.query(target, keys).value
-  },
+export function parse(str: string): PublicKey[] {
+  return str.split(r)
+    .filter(item => item.trim())
+    .map(item => item[0] === '.' ? item.substr(1) : (item[0] === '[' ? + item.substr(1, item.length - 2) : item))
 }
-  
-export default keySerializer
+export function stringify(arr: PublicKey[]): string {
+  return arr.reduce((memo, item, i) =>
+    memo += typeof item === 'number' ? `[${item}]` : i === 0 ? item : `.${item}`
+  , '') as string
+}
+export function query(target: any, keys?: string | PublicKey[] | null) {
+  if (keys == null || keys === '') {
+    return { target, key: undefined, value: target }
+  }
+  if (typeof keys === 'string') {
+    keys = parse(keys) as PublicKey[]
+  } else {
+    keys = keys.slice() as PublicKey[]
+  }
+  let value = target
+  let key: PublicKey | undefined
+  while (keys.length) {
+    target = value
+    key = keys.shift()!
+    // this line may throw when target is undefined, and i do not need handle
+    value = target[key]
+  }
+  return { target, key, value }
+}
+export function get(target: object, keys: string | PublicKey[]): any {
+  return query(target, keys).value
+}
+export function set(target: object, keys: string | PublicKey[], value: any): void {
+  const result = query(target, keys)
+  if (result.key !== undefined) {
+    result.target[result.key] = value
+  }
+}
+export default { stringify, parse, query, get, set }

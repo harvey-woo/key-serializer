8a31779d89a726d361e243187c9b0158cede0767
Harvey Woo <harvey.woo@outlook.com>
2019-03-23 22:24:38 +0800

    chore: remove current tag detect

--- merge.sh
+++ merge.sh
@@ -11,32 +11,25 @@ TO_BRANCH="master"
 
 # Get the current branch
 export PAGER=cat
-CURRENT_BRANCH=$(git log -n 1 --pretty=%d HEAD | cut -d"," -f3 | cut -d" " -f2 | cut -d")" -f1)
-echo "current branch is '$CURRENT_BRANCH'"
 
 # Create the URL to push merge to 
 URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
 echo "Repo url is $URL"
 PUSH_URL="https://$GITHUB_TOKEN:$GIT_PASS@${URL:6}"
+# Checkout the dev branch
+#git checkout $FROM_BRANCH && \
+#echo "Checking out $TO_BRANCH..." && \
 
-if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
-    # Checkout the dev branch
-    #git checkout $FROM_BRANCH && \
-    #echo "Checking out $TO_BRANCH..." && \
+# Checkout the latest stable
+git fetch origin $TO_BRANCH:$TO_BRANCH && \
+git checkout $TO_BRANCH && \
 
-    # Checkout the latest stable
-    git fetch origin $TO_BRANCH:$TO_BRANCH && \
-    git checkout $TO_BRANCH && \
+# Merge the dev into latest stable
+echo "Merging changes..." && \
+git merge $FROM_BRANCH && \
 
-    # Merge the dev into latest stable
-    echo "Merging changes..." && \
-    git merge $FROM_BRANCH && \
-
-    # Push changes back to remote vcs
-    echo "Pushing changes..." && \
-    git push $PUSH_URL && \
-    echo "Merge complete!" || \
-    echo "Error Occurred. Merge failed"
-else
-    echo "Not on $FROM_BRANCH. Skipping merge"
-fi
+# Push changes back to remote vcs
+echo "Pushing changes..." && \
+git push $PUSH_URL && \
+echo "Merge complete!" || \
+echo "Error Occurred. Merge failed"
